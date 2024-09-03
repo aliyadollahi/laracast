dd7356317ee324ff678713335238006f5acf2489
aliyadollahi <ali.yadollahi7074@gmail.com>
2024-09-03 15:23:57 +0330

    end of days 10

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            
+
         ];
     }
 
@@ -42,4 +44,5 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
 }
